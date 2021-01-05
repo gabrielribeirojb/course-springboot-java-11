893f5811d2f5155cd45b72a44ab8e96a45a8bad3
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-01-05 17:12:14 -0300

    Exception handling - findById

--- src/main/java/com/webigode/webservicesspring/services/UserService.java
+++ src/main/java/com/webigode/webservicesspring/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.webigode.webservicesspring.entities.User;
 import com.webigode.webservicesspring.repositories.UserRepository;
+import com.webigode.webservicesspring.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
